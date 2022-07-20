294ee64e9b131b35aecd5a10cdf1dae4449879a1
chohyuntae <chohyuntae@realityreflection.com>
2022-07-20 11:09:59 +0900

    init thug_war project

---
+++ thug_war/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1471300.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 93486037, 93611369, 93612872, 93615538, 93618639, 93618849, 93620883, 93622713, 93628355, 93864332, 93868454, 93874119 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
