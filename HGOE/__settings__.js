ceca7223d9090a398a722a86a1ca57537b29ba15
chohyuntae <chohyuntae@realityreflection.com>
2022-08-02 18:54:14 +0900

    할리갈리 홀짝 프로토 타이핑 완료

---
+++ HGOE/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1486069.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 95475238, 95475260, 95484383, 95533143, 95544738, 95558863, 95558915, 95563938, 95563990, 95734558, 95736454, 95961149 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
