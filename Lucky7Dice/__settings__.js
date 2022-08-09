e5358629c901d7de0959926077e4039c12cc1a78
chohyuntae <chohyuntae@realityreflection.com>
2022-08-09 13:02:38 +0900

    럭키7다이스 프로토 타이핑 완료

---
+++ Lucky7Dice/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1495189.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 96690117, 96690120, 96690106, 96690133, 96690128, 96690109, 96690136, 96690123, 96690107, 96690122, 96690126, 96690113 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
