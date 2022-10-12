d8e038209da58e2a671898b1b2cc8e781e979493
chohyuntae <chohyuntae@realityreflection.com>
2022-10-12 12:03:22 +0900

    CCR 프로토 타이핑 완료

---
+++ CCR/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1550519.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 104683653, 104683661, 104683644, 104683629, 104683676, 104683654, 104683633, 104683670, 104683656, 104683674, 104683631, 104683637, 104683659, 104683630, 104683666, 104860683, 104875377 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
