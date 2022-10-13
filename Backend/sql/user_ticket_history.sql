868e19bbfc5fdf28e93f540797c89b8942faa6df
YD <yd@rr.cool>
2022-10-13 22:50:07 +0900

    userId Type

--- Backend/sql/user_ticket_history.sql
+++ Backend/sql/user_ticket_history.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `user_ticket_history` (
-  `userId` int NOT NULL,
+  `userId` varchar(32) NOT NULL,
   `ticketId` varchar(36) NOT NULL,
   PRIMARY KEY (`userId`,`ticketId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
