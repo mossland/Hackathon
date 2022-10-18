d5a17feecfe9b1f4f4238aded3e584e3a129aa43
Chohyuntae <chohyuntae@realityreflection.com>
2022-10-18 13:22:17 +0900

    Higher 프로토 타이핑 완료

---
+++ Higher/README.md
@@ -0,0 +1,6 @@
+# Higher
+- 
+```
+1. 
+```
+[Demo Page](http://asset.moss.land/Higher/index.html)
---
+++ Higher/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1559734.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 105993486, 105993500, 105993481, 105993496, 105993503, 105993508, 105993497, 105993474, 105993482, 105993484, 105993491, 105993506, 105993483, 105993492, 105993480 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
