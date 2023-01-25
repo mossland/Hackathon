1e485879126ad06980562ab43f505b9a90e9f73b
Wooram Son <wooram.son@gmail.com>
2023-01-25 17:52:52 +0900

    test code

---
+++ provably_fair_implementation/provably_fair/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace provably_fair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new FairServer();
+            var hash = server.StartSession();
+            Console.WriteLine(string.Join("", hash.Select(x => x.ToString("X2"))));
+
+            for (int i = 0; i < 10; i++)
+            {
+                var roll = server.Roll("My Key");
+                Console.WriteLine("Message: {0} Result: {1}", roll.HmacMessage, roll.Roll);
+            }
+
+            var key = server.EndSession();
+            Console.WriteLine(string.Join("", key.Select(x => x.ToString("X2"))));
+            Console.ReadLine();
+        }
+    }
+}
