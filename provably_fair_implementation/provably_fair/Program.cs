0120f9ae8c7a11bc2b6577e8fae60275a760c62c
Wooram Son <wooram.son@gmail.com>
2023-01-25 18:03:23 +0900

    Update Program.cs

--- provably_fair_implementation/provably_fair/Program.cs
+++ provably_fair_implementation/provably_fair/Program.cs
@@ -9,16 +9,16 @@ namespace provably_fair
         {
             var server = new FairServer();
             var hash = server.StartSession();
-            Console.WriteLine(string.Join("", hash.Select(x => x.ToString("X2"))));
+            Console.WriteLine("Hashed server seed: " + string.Join("", hash.Select(x => x.ToString("X2"))));
 
             for (int i = 0; i < 10; i++)
             {
                 var roll = server.Roll("My Key");
-                Console.WriteLine("Message: {0} Result: {1}", roll.HmacMessage, roll.Roll);
+                Console.WriteLine("Client seed + Nonce: {0}, Result: {1}", roll.HmacMessage, roll.Roll);
             }
 
             var key = server.EndSession();
-            Console.WriteLine(string.Join("", key.Select(x => x.ToString("X2"))));
+            Console.WriteLine("Server Seed: " + string.Join("", key.Select(x => x.ToString("X2"))));
             Console.ReadLine();
         }
     }
