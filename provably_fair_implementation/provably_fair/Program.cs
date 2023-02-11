c9687b1d5deb1929e574a5ecd5db3c3372d376f8
Wooram Son <wooram.son@gmail.com>
2023-02-11 10:50:05 +0900

    Reverse Hash Chain

--- provably_fair_implementation/provably_fair/Program.cs
+++ provably_fair_implementation/provably_fair/Program.cs
@@ -7,8 +7,11 @@ namespace provably_fair
     {
         static void Main(string[] args)
         {
+            // Test #1
             var server = new FairServer();
             var hash = server.StartSession();
+
+            Console.WriteLine("== Test #1 ==");
             Console.WriteLine("Hashed server seed: " + string.Join("", hash.Select(x => x.ToString("X2"))));
 
             for (int i = 0; i < 10; i++)
@@ -19,7 +22,21 @@ namespace provably_fair
 
             var key = server.EndSession();
             Console.WriteLine("Server Seed: " + string.Join("", key.Select(x => x.ToString("X2"))));
-            Console.ReadLine();
+
+
+            // Test #2
+            var rhc = new ReverseHashChain(10, 100);
+
+            Console.WriteLine("");
+            Console.WriteLine("== Test #2 ==");
+
+
+            for (int i = 0; i < rhc.hashList.Count; i++)
+            {
+                // hash-chain 에서 결과 가져오기 
+                ReverseHashChainResult rhcr = rhc.hashList[i];
+                Console.WriteLine("Index: {0}, Random: {1}, Hash: {2}", i, rhcr.random, string.Join("", rhcr.seed.Select(x => x.ToString("X2"))));
+            }
         }
     }
 }
---
+++ provably_fair_implementation/provably_fair/ReverseHashChain.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace provably_fair
+{
+    public class ReverseHashChainResult
+    {
+        public int index { get; }
+        public byte[] seed { get; }
+        public float? random { get; }
+
+        public ReverseHashChainResult(int index, byte[] seed, float? random)
+        {
+            this.index = index;
+            this.seed = seed;
+            this.random = random;
+        }
+    }
+
+    public class ReverseHashChain
+    {
+        private int hashSize = 100;
+
+        private byte[] salt;
+        private byte[] seed;
+
+        public List<ReverseHashChainResult> hashList { get; }
+
+        public ReverseHashChain(int hashSize, int maxRandomValue)
+        {
+            // Generate a new seed hash.
+            var rng = RandomNumberGenerator.Create();
+
+            seed = new byte[128];
+            rng.GetBytes(seed);
+
+            salt = new byte[128];
+            rng.GetBytes(salt);
+
+            // Create a reverse hash-chain.
+            byte[] hash = seed;
+
+            var hmac = new HMACSHA256(salt);
+            hashList = new List<ReverseHashChainResult>();
+
+            for (int i = 0; i < hashSize; i++)
+            {
+                ReverseHashChainResult result = new ReverseHashChainResult(hashSize - i, hash, GetNumberFromByteArray(hash) * maxRandomValue);
+                hashList.Add(result);
+
+                hash = hmac.ComputeHash(hash);
+            }
+
+            hashList.Reverse();
+        }
+
+        private float? GetNumberFromByteArray(byte[] hash)
+        {
+            var hashString = string.Join("", hash.Select(x => x.ToString("X2")));
+            const int chars = 5;
+            for (int i = 0; i <= hashString.Length - chars; i += chars)
+            {
+                var substring = hashString.Substring(i, chars);
+                var number = int.Parse(substring, System.Globalization.NumberStyles.HexNumber);
+                if (number > 999999)
+                    continue;
+                return (number % 10000) / 10000.0f;
+            }
+            return null;
+        }
+    }
+}
+
