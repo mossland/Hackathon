3647b14a1636c4176f3248bc80189697f71e97d5
Chohyuntae <chohyuntae815@gmail.com>
2023-06-21 18:31:09 +0900

    ccr tween update

--- CCR/__settings__.js
+++ CCR/__settings__.js
@@ -1,21 +1,21 @@
-ASSET_PREFIX = "";
-SCRIPT_PREFIX = "";
-SCENE_PATH = "1550519.json";
-CONTEXT_OPTIONS = {
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1550519.json";
+window.CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': false,
     'preserveDrawingBuffer': false,
     'preferWebGl2': true,
     'powerPreference': "high-performance"
 };
-SCRIPTS = [ 104683653, 104683661, 104683644, 104683629, 104683676, 104683654, 104683633, 104683670, 104683656, 104683674, 104683631, 104683637, 104683659, 104683630, 104683666, 104860683, 104875377 ];
-CONFIG_FILENAME = "config.json";
-INPUT_SETTINGS = {
+window.SCRIPTS = [ 104683653, 104683661, 104683644, 104683629, 104683676, 104683654, 104683633, 104683670, 104683656, 104683674, 104683631, 104683637, 104683659, 104683630, 104683666, 104860683, 104875377 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
     useGamepads: false,
     useTouch: true
 };
 pc.script.legacy = false;
-PRELOAD_MODULES = [
+window.PRELOAD_MODULES = [
 ];
