1d3c9dd4dffb525cae719cec93930021d350e0ca
yd <yd@moss.land>
2023-08-24 18:33:06 +0900

    handle window

--- game-portal-web/src/app/Storage.tsx
+++ game-portal-web/src/app/Storage.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-const { useEffect } = require("react");
-
-
 export default function Storage({ token }: { token: string}) {
-    useEffect(() => {
+    if (typeof window !== "undefined") {
         if (token && window.sessionStorage.getItem('token') !== token) {
             window.sessionStorage.setItem('token', token);
         }
-    }, [window.sessionStorage.getItem('token')]);
+    }
     
     return <></>;
 }
