058cb607049db7d2c22038f2c4466aa13181c3c8
yd <yd@moss.land>
2023-08-24 18:58:21 +0900

    Storage

--- game-portal-web/src/app/Storage.tsx
+++ game-portal-web/src/app/Storage.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+const { useEffect } = require("react");
+
+
 export default function Storage({ token }: { token: string}) {
-    if (typeof window !== "undefined") {
+    useEffect(() => {
         if (token && window.sessionStorage.getItem('token') !== token) {
             window.sessionStorage.setItem('token', token);
         }
-    }
+    }, [window.sessionStorage.getItem('token')]);
     
     return <></>;
 }
--- game-portal-web/src/app/page.tsx
+++ game-portal-web/src/app/page.tsx
@@ -1,11 +1,11 @@
 import dynamic from 'next/dynamic';
 
-import Storage from './Storage';
 import { GameListViewLoading } from './GameListView';
 
 export default function Home({ searchParams }: { searchParams: { token?: string } }) {
   const token = searchParams.token ?? '';
   const GameListView = dynamic(() => import('./GameListView'), { ssr: false, loading: GameListViewLoading });
+  const Storage = dynamic(() => import('./Storage'), { ssr: false });
 
   return (
     <>
