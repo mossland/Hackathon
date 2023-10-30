400a8a4314c7da98a0dffc7e674f8e3959c209ad
Chohyuntae <chohyuntae815@gmail.com>
2023-10-30 13:37:36 +0900

    one, two, three init

---
+++ OneTwoThree/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1882372.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 154122863, 154122878, 154122826, 154122844, 154122831, 154122839, 154122820, 154122835, 154122880, 154122884, 154122840, 154122861, 154122885, 154122823, 154122825, 154122848, 154122828, 154122842, 154122829, 154122849, 154122846, 154122843, 154142061, 154653300 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
