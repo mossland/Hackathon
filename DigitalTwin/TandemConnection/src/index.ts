08157ba1585921676d2c8dcc5f1d64d407206676
YD <zootyducky@gmail.com>
2025-05-12 17:06:43 +0900

    send connection data to sample facility

--- DigitalTwin/TandemConnection/src/index.ts
+++ DigitalTwin/TandemConnection/src/index.ts
@@ -2,14 +2,17 @@ require('dotenv').config({
     path: '.env'
 });
 
+import fs from 'fs';
 import bluebird from 'bluebird';
 import axios from 'axios';
 import dayjs from 'dayjs';
+import csvParser from 'csv-parser';
+import Big from 'big.js';
 
-const led_b = process.env.LED_B_CONNECTION;
-const led_g = process.env.LED_G_CONNECTION;
-const led_r = process.env.LED_R_CONNECTION;
-const led_sw = process.env.LED_SW_CONNECTION;
+// const led_b = process.env.LED_B_CONNECTION;
+// const led_g = process.env.LED_G_CONNECTION;
+// const led_r = process.env.LED_R_CONNECTION;
+// const led_sw = process.env.LED_SW_CONNECTION;
 
 class SmoothRandomTimeSeriesInt {
     private value: number;
@@ -25,52 +28,94 @@ class SmoothRandomTimeSeriesInt {
     }
 
     next(): number {
-        const delta = Math.floor(Math.random() * (2 * this.stepSize + 1)) - this.stepSize;
+        const ran = Math.random();
+        const delta = ran < 0.33 ? -this.stepSize : ran < 0.66 ? 0 : this.stepSize;
         this.value += delta;
         this.value = Math.max(this.min, Math.min(this.max, this.value));
-        return this.value;
+        return Big(this.value).round(2, 0).toNumber();
     }
 }
 
-const led_b_series = new SmoothRandomTimeSeriesInt(0, 1, 0, 100);
-const led_g_series = new SmoothRandomTimeSeriesInt(0, 1, 0, 100);
-const led_r_series = new SmoothRandomTimeSeriesInt(0, 1, 0, 100);
-const led_sw_series = new SmoothRandomTimeSeriesInt(0, 1, 0, 100);
+// const led_b_series = new SmoothRandomTimeSeriesInt(0, 1, 0, 100);
+// const led_g_series = new SmoothRandomTimeSeriesInt(0, 1, 0, 100);
+// const led_r_series = new SmoothRandomTimeSeriesInt(0, 1, 0, 100);
+// const led_sw_series = new SmoothRandomTimeSeriesInt(0, 1, 0, 100);
 
-const urlAndSeries = [
-    { url: led_b, series: led_b_series, key: 'led_b_brightness' },
-    { url: led_g, series: led_g_series, key: 'led_g_brightness' },
-    { url: led_r, series: led_r_series, key: 'led_r_brightness' },
-    { url: led_sw, series: led_sw_series, key: 'led_sw_pr' },
-];
+// const urlAndSeries = [
+//     { url: led_b, series: led_b_series, key: 'led_b_brightness' },
+//     { url: led_g, series: led_g_series, key: 'led_g_brightness' },
+//     { url: led_r, series: led_r_series, key: 'led_r_brightness' },
+//     { url: led_sw, series: led_sw_series, key: 'led_sw_pr' },
+// ];
 
-setInterval(async () => {
-    await bluebird.each(
-        urlAndSeries,
-        async ({ url, series, key }) => {
-            const value = series.next();
-            console.log(url, key, value);
-            try {
-                const pw = url.split(":")[2].split("@")[0]
-                const { data } = await axios.post(
-                    url,
-                    { [key]: value, timeStamp: dayjs().toISOString() },
-                    {
-                        headers: { 'Content-Type': 'application/json' },
-                        auth: {
-                            username: '',
-                            password: pw
-                        }
+const interval = 50000;
 
-                    }
-                );
-                console.log(data);
-                console.log(`${key} : ${value} sent to ${key}`);
-            } catch (error) {
-                console.error(error);
-            }
+(async () => {
+    const rawConnections: any[] = await new Promise((resolve, reject) => {
+        const connections: any[] = [];
+        fs.createReadStream('connections_export.csv')
+            .pipe(csvParser())
+            .on('data', (row) => {
+                connections.push(row);
+            })
+            .on('end', () => {
+                resolve(connections);
+            });
+    });
+
+    const urlAndSeriese = rawConnections.map((connection) => {
+        return {
+            url: connection.ingestionUrl,
+            airFlowSeries: new SmoothRandomTimeSeriesInt(20, 0.5, 10, 30),
+            humiditySeries: new SmoothRandomTimeSeriesInt(40, 0.5, 25, 60),
+            occupancySeries: new SmoothRandomTimeSeriesInt(0, 1, 0, 2),
+            temperatureSeries: new SmoothRandomTimeSeriesInt(70, 0.1, 66, 82),
         }
-    );
-}, 10000);
+    });
+
+
+    setInterval(async () => {
+        console.log(`Start ${dayjs().format('YYYY-MM-DD HH:mm:ss')}`);
+        await bluebird.map(
+            urlAndSeriese,
+            async ({ url, airFlowSeries, humiditySeries, occupancySeries, temperatureSeries }, index) => {
+                console.log(`${index + 1} of ${urlAndSeriese.length}`);
+                const occNext = occupancySeries.next();
+                const body = {
+                    air_flow: airFlowSeries.next(),
+                    humidity: humiditySeries.next(),
+                    occupancy: occNext === 0 ? 'Free' : occNext === 1 ? 'Occupied' : 'Unavailable',
+                    temperature: temperatureSeries.next(),
+                    ts: dayjs().toISOString()
+                };
+                try {
+                    const pw = url.split(":")[2].split("@")[0]
+                    const { data } = await axios.post(
+                        url,
+                        body,
+                        {
+                            headers: { 'Content-Type': 'application/json' },
+                            auth: {
+                                username: '',
+                                password: pw
+                            }
+
+                        }
+                    );
+                    console.log(`${body.air_flow} (CFM - airflow) : ${body.humidity} (%RH - humidity) : ${body.occupancy} (occupancy) : ${body.temperature} (F - temperature) sent to ${url}`);
+                } catch (error) {
+                    console.error(error);
+                }
+            },
+            { concurrency: 10 }
+        );
+        console.log(`End ${dayjs().format('YYYY-MM-DD HH:mm:ss')}`);
+        console.log(`Next run in ${interval / 1000} seconds`);
+    }, interval);
+})();
+
+
+
+
 
 
